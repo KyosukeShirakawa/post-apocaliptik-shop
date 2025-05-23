59b20ef69625b1bc18b7302aa4e96b239d92b258
Kyosuke Shirakawa <mr.kyosuke0527@gmail.com>
2025-05-23 15:40:04 +0200

    index.php styling

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer class="bg-green-700 py-3 rounded-sm">
+  <p class="text-center">&copy; 2025 PostApocaliptIK Shop</p>
+</footer>
+
+</html>
---
+++ header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="./includes/dist/output.css" rel="stylesheet">
+  <title>Document</title>
+</head>
+
+<body>
+  <header class="bg-green-700 px-5 py-2 mb-5 rounded-sm">
+    <h1 class="text-xl text-white font-bold">PostApocaliptIK Shop</h1>
+    <div class="flex justify-between text-amber-300">
+      <div>
+        <button>Home</button>
+        <button>Add New Item</button>
+      </div>
+      <div>
+        <button>Cart</button>
+        <button>Login</button>
+        <button>Register</button>
+      </div>
+    </div>
+  </header>
+
+</body>
